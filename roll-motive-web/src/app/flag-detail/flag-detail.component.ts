d1b0ed88d02f1710ee6049aca79e184c3703510c
Anas Siddiqi <anas.siddiqui@keeptruckin.com>
2022-07-21 17:17:27 +0500

    Add rules to detail page

--- roll-motive-web/src/app/flag-detail/flag-detail.component.ts
+++ roll-motive-web/src/app/flag-detail/flag-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HeaderService } from '../header.service';
 
 @Component({
@@ -8,6 +8,14 @@ import { HeaderService } from '../header.service';
 })
 export class FlagDetailComponent implements OnInit {
   public flagDetail: any;
+  public ruleAttribute: string = 'company_id';
+  public ruleRelation: string = 'is_one_of';
+  public ruleValue: boolean = true;
+  @ViewChild('inputElement', { static: false }) inputElement?: ElementRef;
+
+  tags = ['160', '140', '424790'];
+  inputVisible = false;
+  inputValue = '';
 
   constructor(private headerService: HeaderService) { }
 
@@ -30,5 +38,30 @@ export class FlagDetailComponent implements OnInit {
 
   public onFlagStatusChanged(value: boolean) {
     console.log(value);
-  } 
+    console.log(this.ruleAttribute);
+  }
+
+  handleClose(removedTag: {}): void {
+    this.tags = this.tags.filter(tag => tag !== removedTag);
+  }
+
+  sliceTagName(tag: string): string {
+    const isLongTag = tag.length > 20;
+    return isLongTag ? `${tag.slice(0, 20)}...` : tag;
+  }
+
+  showInput(): void {
+    this.inputVisible = true;
+    setTimeout(() => {
+      this.inputElement?.nativeElement.focus();
+    }, 10);
+  }
+
+  handleInputConfirm(): void {
+    if (this.inputValue && this.tags.indexOf(this.inputValue) === -1) {
+      this.tags = [...this.tags, this.inputValue];
+    }
+    this.inputValue = '';
+    this.inputVisible = false;
+  }
 }
